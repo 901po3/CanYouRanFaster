1b56e6b6a597aadd9981c234cc3f596de0e0da21
Hyukin Kown <901po3@gmail.com>
2020-02-29 01:27:57 -0500

    ledgeMove

--- Assets/Scripts/Character/States/StateScripts/MoveStateScripts/CanMoveLedgeHorizontal.cs
+++ Assets/Scripts/Character/States/StateScripts/MoveStateScripts/CanMoveLedgeHorizontal.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New State", menuName = "Hyukin's_Game/AbilityData/LedgeMoveHorizontal")]
-public class LedgeMoveHorizontal : MovingStateData
+[CreateAssetMenu(fileName = "New State", menuName = "Hyukin's_Game/AbilityData/CanMoveLedgeHorizontal")]
+public class CanMoveLedgeHorizontal : MovingStateData
 {
     public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
     {
@@ -13,17 +13,16 @@ public class LedgeMoveHorizontal : MovingStateData
     public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
     {
         CharacterControl charControl = characterState.GetCharacterControl(animator);
-
-        if (!charControl.isMoving)
+        if (charControl.ledgeCheckers[0].grabbedLedge == null || charControl.ledgeCheckers[1].grabbedLedge == null)
         {
-            animator.SetTrigger("LedgeMoveRestriced");
+            animator.SetTrigger("manualFall");
             return;
         }
 
         RoateToCamFacingDir(charControl);
         float curSpeed = CalculateSpeed(charControl, stateInfo);
 
-        LedgeMove(charControl, curSpeed);
+        animator.SetBool("isLedgeHorizontalMove",CanLedgeMove(charControl, curSpeed));
     }
 
     public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
@@ -31,46 +30,37 @@ public class LedgeMoveHorizontal : MovingStateData
 
     }
 
-    private void LedgeMove(CharacterControl charControl, float curSpeed)
+    private bool CanLedgeMove(CharacterControl charControl, float curSpeed)
     {
         Transform ledgeTrans = charControl.ledgeCheckers[0].grabbedLedge.transform;
         if (charControl.isMovingLeft)
         {
             Vector3 leftHandEdge = (-charControl.ledgeCheckers[0].transform.right * getWorldScaleOfX(charControl.ledgeCheckers[0].transform) / 2) + charControl.ledgeCheckers[0].transform.position;
             Vector3 ledgeLeftEdge = ledgeTrans.position + (-ledgeTrans.right * getWorldScaleOfX(ledgeTrans) / 2);
-            Debug.DrawRay(leftHandEdge, Vector3.up * 10, Color.yellow);
-            Debug.DrawRay(ledgeLeftEdge, Vector3.up * 10, Color.yellow);
-            if (Vector3.Distance(leftHandEdge, ledgeLeftEdge) > 0.2f)
+            leftHandEdge += -charControl.transform.right * curSpeed * Time.deltaTime;
+            if (Vector3.Distance(leftHandEdge, ledgeLeftEdge) > 0.275f)
             {
-                charControl.transform.Translate(Vector3.left * curSpeed * Time.deltaTime);
-            }
-            else
-            {
-                charControl.GetComponent<Animator>().SetTrigger("LedgeMoveRestriced");
+                return true;
             }
         }
-        else if(charControl.isMovingRight)
+        else if (charControl.isMovingRight)
         {
             Vector3 rightHandEdge = (charControl.ledgeCheckers[1].transform.right * getWorldScaleOfX(charControl.ledgeCheckers[1].transform) / 2) + charControl.ledgeCheckers[1].transform.position;
             Vector3 ledgeRightEdge = ledgeTrans.position + (ledgeTrans.right * getWorldScaleOfX(ledgeTrans) / 2);
-            Debug.DrawRay(rightHandEdge, Vector3.up * 10);
-            Debug.DrawRay(ledgeRightEdge, Vector3.up * 10);
-            if (Vector3.Distance(rightHandEdge, ledgeRightEdge) > 0.2f)
+            rightHandEdge += charControl.transform.right * curSpeed * Time.deltaTime;
+            if (Vector3.Distance(rightHandEdge, ledgeRightEdge) > 0.275f)
             {
-                charControl.transform.Translate(Vector3.right * curSpeed * Time.deltaTime);
-            }
-            else
-            {
-                charControl.GetComponent<Animator>().SetTrigger("LedgeMoveRestriced");
+                return true;
             }
         }
+        return false;
     }
 
     private float getWorldScaleOfX(Transform trans)
     {
         float x = trans.localScale.x;
         Transform parentTrans = trans;
-        while(true)
+        while (true)
         {
             if (parentTrans.parent != null)
             {
---
+++ Assets/Scripts/Character/States/StateScripts/MoveStateScripts/LedgeMoveHorizontal.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New State", menuName = "Hyukin's_Game/AbilityData/LedgeMoveHorizontal")]
+public class LedgeMoveHorizontal : MovingStateData
+{
+    public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+    {
+
+    }
+
+    public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+    {
+        CharacterControl charControl = characterState.GetCharacterControl(animator);
+
+        if (!charControl.isMoving)
+        {
+            animator.SetBool("isLedgeHorizontalMove", false);
+            return;
+        }
+
+        RoateToCamFacingDir(charControl);
+        float curSpeed = CalculateSpeed(charControl, stateInfo);
+
+        LedgeMove(charControl, animator, curSpeed);
+    }
+
+    public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+    {
+
+    }
+
+    private void LedgeMove(CharacterControl charControl, Animator animator, float curSpeed)
+    {
+        Transform ledgeTrans = charControl.ledgeCheckers[0].grabbedLedge.transform;
+        if (charControl.isMovingLeft)
+        {
+            Vector3 leftHandEdge = (-charControl.ledgeCheckers[0].transform.right * getWorldScaleOfX(charControl.ledgeCheckers[0].transform) / 2) + charControl.ledgeCheckers[0].transform.position;
+            Vector3 ledgeLeftEdge = ledgeTrans.position + (-ledgeTrans.right * getWorldScaleOfX(ledgeTrans) / 2);
+            Debug.DrawRay(leftHandEdge, Vector3.up * 10, Color.yellow);
+            Debug.DrawRay(ledgeLeftEdge, Vector3.up * 10, Color.yellow);
+            if (Vector3.Distance(leftHandEdge, ledgeLeftEdge) > 0.25f)
+            {
+                charControl.transform.Translate(Vector3.left * curSpeed * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("isLedgeHorizontalMove", false);
+            }
+        }
+        else if(charControl.isMovingRight)
+        {
+            Vector3 rightHandEdge = (charControl.ledgeCheckers[1].transform.right * getWorldScaleOfX(charControl.ledgeCheckers[1].transform) / 2) + charControl.ledgeCheckers[1].transform.position;
+            Vector3 ledgeRightEdge = ledgeTrans.position + (ledgeTrans.right * getWorldScaleOfX(ledgeTrans) / 2);
+            Debug.DrawRay(rightHandEdge, Vector3.up * 10);
+            Debug.DrawRay(ledgeRightEdge, Vector3.up * 10);
+            rightHandEdge += charControl.transform.right * curSpeed * Time.deltaTime;
+            if (Vector3.Distance(rightHandEdge, ledgeRightEdge) > 0.25f)
+            {
+                charControl.transform.Translate(Vector3.right * curSpeed * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("isLedgeHorizontalMove", false);
+            }
+        }
+    }
+
+    private float getWorldScaleOfX(Transform trans)
+    {
+        float x = trans.localScale.x;
+        Transform parentTrans = trans;
+        while(true)
+        {
+            if (parentTrans.parent != null)
+            {
+                x *= trans.parent.localScale.x;
+                parentTrans = parentTrans.parent;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return x;
+    }
+}
