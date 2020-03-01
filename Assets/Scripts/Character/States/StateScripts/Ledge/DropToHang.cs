4d6936b6e363e38778babdeabe6231d5c1ac1c03
Hyukin Kown <901po3@gmail.com>
2020-03-01 01:37:41 -0500

    1

--- Assets/Scripts/Character/States/StateScripts/Ledge/DropToHang.cs
+++ Assets/Scripts/Character/States/StateScripts/Ledge/DropToHang.cs
@@ -19,6 +19,7 @@ public class DropToHang : StateData
             anim.transform.localPosition = new Vector3(anim.transform.localPosition.x + charControl.bottomLedge.DropToHangOffset.x, charControl.bottomLedge.DropToHangOffset.y, charControl.bottomLedge.DropToHangOffset.z);
             anim.transform.localRotation = Quaternion.Euler(0, 180, 0);
             anim.transform.parent = originParent;
+            charControl.transform.localScale = new Vector3(1, 1, 1);
             charControl.RIGIDBODY.velocity = Vector3.zero;
             charControl.RIGIDBODY.useGravity = false;
         }
@@ -31,17 +32,19 @@ public class DropToHang : StateData
         charControl.ledgeCheckers[1].grabbedLedge = ledge;
         charControl.ledgeCheckers[0].isGrabbingLedge = true;
         charControl.ledgeCheckers[1].isGrabbingLedge = true;
+        charControl.transform.localScale = new Vector3(1, 1, 1);
     }
 
     public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
     {
-        CharacterControl charControl = characterState.GetCharacterControl(animator);
-        GameObject anim = charControl.gameObject;
-        Transform originParent = anim.transform.parent;
-        anim.transform.parent = charControl.bottomLedge.transform;
-        anim.transform.localPosition = new Vector3(anim.transform.localPosition.x + charControl.bottomLedge.DropToHangEndOffset.x, charControl.bottomLedge.DropToHangEndOffset.y, charControl.bottomLedge.DropToHangEndOffset.z);
-        anim.transform.localRotation = Quaternion.Euler(0, 0, 0);
-        anim.transform.parent = originParent;
-        charControl.RIGIDBODY.velocity = Vector3.zero;
+        //CharacterControl charControl = characterState.GetCharacterControl(animator);
+        //GameObject anim = charControl.gameObject;
+        //Transform originParent = anim.transform.parent;
+        //anim.transform.parent = charControl.bottomLedge.transform;
+        //anim.transform.localPosition = new Vector3(anim.transform.localPosition.x + charControl.bottomLedge.DropToHangEndOffset.x, charControl.bottomLedge.DropToHangEndOffset.y, charControl.bottomLedge.DropToHangEndOffset.z);
+        //anim.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        //anim.transform.parent = originParent;
+        //charControl.RIGIDBODY.velocity = Vector3.zero;
+        //charControl.transform.localScale = new Vector3(1, 1, 1);
     }
 }
