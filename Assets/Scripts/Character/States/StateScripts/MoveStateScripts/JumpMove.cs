2e2fcb9d591682e9c6e99643a96cc7bfa9f0bc0d
Hyukin Kown <901po3@gmail.com>
2020-03-01 22:30:32 -0500

    1

--- Assets/Scripts/Character/States/StateScripts/MoveStateScripts/JumpMove.cs
+++ Assets/Scripts/Character/States/StateScripts/MoveStateScripts/JumpMove.cs
@@ -5,50 +5,58 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New State", menuName = "Hyukin's_Game/AbilityData/JumpMove")]
 public class JumpMove : MovingStateData
 {
+    Vector3 momentum;
+    public float momentumSpeed;
+    public float momentumMaxPower;
     public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
     {
-
+        momentum = Vector3.zero;
     }
 
     public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
     {
         CharacterControl charControl = characterState.GetCharacterControl(animator);
 
-        if (charControl.isJumping)
-        {
-            animator.SetBool("isJumping", true);
-        }
-
-        if (!charControl.isMoving)
-        {
-            animator.SetBool("isRunning", false);
-            return;
-        }
-
         RoateToCamFacingDir(charControl);
         float curSpeed = CalculateSpeed(charControl, stateInfo);
-        charControl.velocity = Vector3.zero;
 
-        if (charControl.isMovingForward && !CheckEdge(charControl, charControl.frontSpheres, charControl.transform.forward))
+        if (charControl.isMovingForward)
         {
-            charControl.transform.Translate(Vector3.forward * curSpeed * Time.deltaTime);
-            charControl.velocity.z = curSpeed;
+            momentum.z += momentumSpeed * Time.deltaTime;
         }
-        else if (charControl.isMovingBackward && !CheckEdge(charControl, charControl.backSpheres, -charControl.transform.forward))
+        else if (charControl.isMovingBackward)
         {
-            charControl.transform.Translate(Vector3.back * curSpeed * Time.deltaTime);
-            charControl.velocity.z = -curSpeed;
+            momentum.z -= momentumSpeed * Time.deltaTime;
         }
-        if (charControl.isMovingRight && !CheckEdge(charControl, charControl.rightSpheres, charControl.transform.right))
+        if (charControl.isMovingRight)
         {
-            charControl.transform.Translate(Vector3.right * curSpeed * Time.deltaTime);
-            charControl.velocity.x = curSpeed;
+            momentum.x += momentumSpeed * Time.deltaTime;
         }
-        else if (charControl.isMovingLeft && !CheckEdge(charControl, charControl.leftSpheres, -charControl.transform.right))
+        else if (charControl.isMovingLeft)
         {
-            charControl.transform.Translate(Vector3.left * curSpeed * Time.deltaTime);
-            charControl.velocity.x = -curSpeed;
+            momentum.x -= momentumSpeed * Time.deltaTime;
         }
+
+        if(momentum.z != 0)
+        {
+            if (CheckEdge(charControl, charControl.frontSpheres, charControl.transform.forward) || CheckEdge(charControl, charControl.backSpheres, -charControl.transform.forward))
+            {
+                momentum.z = 0;
+            }
+            charControl.transform.Translate(Vector3.forward * momentum.z * Time.deltaTime);
+        }
+        if(momentum.x != 0)
+        {
+            if (CheckEdge(charControl, charControl.rightSpheres, charControl.transform.right) || CheckEdge(charControl, charControl.leftSpheres, -charControl.transform.right))
+            {
+                momentum.x = 0;
+            }
+            charControl.transform.Translate(Vector3.right * momentum.x * Time.deltaTime);
+        }
+
+        momentum.x = Mathf.Clamp(momentum.x, -momentumMaxPower, momentumMaxPower);
+        momentum.z = Mathf.Clamp(momentum.z, -momentumMaxPower, momentumMaxPower);
+        Debug.Log("momentum: " + momentum);
     }
 
     public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
